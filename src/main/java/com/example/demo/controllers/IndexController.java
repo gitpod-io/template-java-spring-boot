cf1905e584f67bf803a357a63298843fb35af230
Nancy Chauhan <nancychn1@gmail.com>
2022-04-22 16:13:32 +0000

    Add Index Controller

---
+++ src/main/java/com/example/demo/controllers/IndexController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        return "Hello World";
+    }
+}
